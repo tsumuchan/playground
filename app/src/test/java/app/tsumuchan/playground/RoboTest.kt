6daee6ada5b0135614359c96221b35719649b0ce
tsumuchan <tsumuchan.app@gmail.com>
2023-12-16 06:26:32 +0900

    add roborazziTest

--- app/src/test/java/app/tsumuchan/playground/RoboTest.kt
+++ app/src/test/java/app/tsumuchan/playground/RoboTest.kt
@@ -2,11 +2,14 @@ package app.tsumuchan.playground
 
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.GraphicsMode
 
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
 @RunWith(RobolectricTestRunner::class)
 class RoboTest {
 
@@ -22,5 +25,16 @@ class RoboTest {
         composeRule
             .onNode(hasText("Hello Robo!"))
             .assertExists()
+    }
+
+    @Test
+    fun roborazziTest() {
+        composeRule.setContent {
+            Greeting(name = "Roborazzi")
+        }
+
+        composeRule
+            .onNode(hasText("Hello Roborazzi!"))
+            .captureRoboImage()
     }
 }
